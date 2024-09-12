b520444a0a63f987fe5d24cd2f5a09282859490c
Niels Jeppesen <niejep@dtu.dk>
2024-09-12 23:54:30 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup, Extension
-
+from setuptools import Extension, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -18,9 +17,11 @@ class LazyCythonize(list):
         for e in self.c_list():
             yield e
 
-    def __getitem__(self, ii): return self.c_list()[ii]
+    def __getitem__(self, ii):
+        return self.c_list()[ii]
 
-    def __len__(self): return len(self.c_list())
+    def __len__(self):
+        return len(self.c_list())
 
 
 def extensions():
@@ -38,29 +39,30 @@ def extensions():
     return cythonize([maxflow_module])
 
 
-setup(name="thinmaxflow",
-      version="0.1.4",
-      author="Niels Jeppesen",
-      author_email="niejep@dtu.dk",
-      description="A thin Maxflow wrapper for Python",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url="https://github.com/Skielex/thinmaxflow",
-      packages=["thinmaxflow"],
-      classifiers=[
-          "Development Status :: 3 - Alpha",
-          "Environment :: Console",
-          "Intended Audience :: Developers",
-          "Intended Audience :: Science/Research",
-          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-          "Natural Language :: English",
-          "Operating System :: OS Independent",
-          "Programming Language :: C++",
-          "Programming Language :: Python",
-          "Topic :: Scientific/Engineering :: Image Recognition",
-          "Topic :: Scientific/Engineering :: Artificial Intelligence",
-          "Topic :: Scientific/Engineering :: Mathematics"
-      ],
-      ext_modules=LazyCythonize(extensions),
-      setup_requires=["Cython"]
-      )
+setup(
+    name="thinmaxflow",
+    version="0.1.5",
+    author="Niels Jeppesen",
+    author_email="niejep@dtu.dk",
+    description="A thin Maxflow wrapper for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Skielex/thinmaxflow",
+    packages=["thinmaxflow"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: C++",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+    ext_modules=LazyCythonize(extensions),
+    setup_requires=["Cython"],
+)
